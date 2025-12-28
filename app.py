dcb5eb71c145278571b8bae9f75d329b27442d90
JitendraSri <talabattulajitendrasri@gmail.com>
2025-12-28 11:36:39 +0530

    Allow Werkzeug for production on Render

--- app.py
+++ app.py
@@ -387,4 +387,9 @@ if __name__ == '__main__':
         print(f"Index creation failed: {e}")
         
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        allow_unsafe_werkzeug=True
+    )
